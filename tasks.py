516209fe140c8cc90eb4efb1d8668d893d3839b4
Adithya Balagoni <127680046+adithya04dev@users.noreply.github.com>
2024-10-25 12:23:50 +0530

    include implementations

--- tasks.py
+++ tasks.py
@@ -30,7 +30,7 @@ use_case_generation_task = Task(
     description=(
         "Undersand previous agent's response and Research and  analyze trends related to Generative or AI/ML in that particular domain/area. and finally generate 4-5 use cases for them. "
         "Research by searching internet,then reading the articles using links,or searching in pdfs . ( Undersatand how you can use use tool for each of given task and call with correct inputs ) "
-        "Generate relevant use cases where the company can leverage GenAI, LLMs, and ML technologies. "
+        "Generate relevant use cases where the company can leverage GenAI, LLMs, and ML technologies and research how it could be implemented by searching about libraries/frameworks "
         "First use the tools by passing  correct input formats..then after reasearching finally return "
         "A list of 4-5 GenAI/AI/ML use cases for this company/industry and description(around 2 lines),and how it could be implemented. about it  {company}\n"
     ),
